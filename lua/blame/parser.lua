d8d0b4da2088099370d9f232a46ebc393d885e8b
Robert Schäfer <git@roschaefer.de>
2026-02-17 21:12:31 +0100

    refactor: `git blame --line-porcelain` for machine readability

--- lua/blame/parser.lua
+++ lua/blame/parser.lua
@@ -5,19 +5,25 @@ local M = {}
 
 function M.parse_blame_output(blame_result_stdout)
 	local parsed_blame_lines = {}
+	local blame_info = {}
 
 	for _, line in ipairs(vim.split(blame_result_stdout, "\n")) do
-		local commit = line:match("^%S+")
-		local rest = line:match("^%S+%s+(.*)")
-		if rest then
-			local author_date, content = rest:match("%((.*)%) (.*)")
-			if author_date and content then
-				local author = author_date:match("^(.*)%s+%d%d%d%d%-%d%d%-%d%d")
-				local date = author_date:match("%d%d%d%d%-%d%d%-%d%d")
-				author = author:gsub("^%s*", ""):gsub("%s*$", "")
-				local blame_info = string.format("%s %s (%s)", commit, author, date)
-				table.insert(parsed_blame_lines, { info = blame_info, commit = commit })
+		-- Silly lua has no regex quantifiers: https://stackoverflow.com/a/32885308
+		local commit = line:match("^%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x")
+		if commit then
+			-- This is the header line for a blame hunk.
+			blame_info.commit = commit
+		elseif line:sub(1, 7) == "author " then
+			blame_info.author = line:sub(8)
+		elseif line:sub(1, 12) == "author-time " then
+			local timestamp = tonumber(line:sub(13))
+			if timestamp then
+				blame_info.date = os.date("%Y-%m-%d", timestamp)
 			end
+		elseif line:sub(1, 1) == "\t" then
+			-- This is the actual file content line, which signifies the end of a blame hunk.
+			table.insert(parsed_blame_lines, blame_info)
+			blame_info = {}
 		end
 	end
 	return parsed_blame_lines
